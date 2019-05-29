010454d808d734e70c5de63510c44be2061df2ee
Muhamad Dwi Arifiant <muhdwi45@gmail.com>
2019-05-29 10:56:53 +0700

    Filter Data Nuxt-Apollo

---
+++ test_filter_nuxt_apollo/nuxt.config.js
@@ -0,0 +1,66 @@
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org/docs/
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/apollo'
+  ],
+
+  apollo: {
+     clientConfigs: {
+      default: {
+        // required  
+        httpEndpoint: 'http://128.199.145.173:8811/graphql',
+      }
+    }
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
---
+++ test_filter_nuxt_apollo/pages/_data_kota.vue
@@ -0,0 +1,51 @@
+<template>
+	<section>
+		<!-- top nav -->
+		<div class="shadow" position="top">
+			<b-navbar variant="info" type="dark">
+			    <b-navbar-nav>
+			      	<b-nav-item href="#">HOME</b-nav-item>
+			      	<b-nav-item href="#">PRODUCT</b-nav-item>
+			      	<b-nav-item href="#">PROFILE</b-nav-item>
+			      	<b-nav-item href="#">HELP</b-nav-item>
+			    </b-navbar-nav>
+		  	</b-navbar>
+		</div>
+
+		<div align="center" class="py-3">
+			<b-form-group class="my-5 w-50">
+	          <b-input-group>
+	            <b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
+	            <b-input-group-append>
+	              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+	            </b-input-group-append>
+	          </b-input-group>
+	        </b-form-group>
+			<b-table class="mb-5 w-75" striped bordered  :items="IDKota" :fields="fields" :filter="filter"></b-table>
+		</div>
+
+		<!-- FOOTER -->
+		<div fluid class="text-center mt-5 bg-info footer-height-0 text-light fixed-bottom">
+			<b>@2019</b>
+		</div>
+	</section>
+</template>
+
+<script>
+	import gql from 'graphql-tag'
+	import dataKota from '~/query/data_kota.gql'
+
+	export default {
+	    apollo: {
+			IDKota:{
+				query: dataKota,
+			},
+	    },
+	    data(){
+	    	return {
+				fields: [{key:'id',sortable:true},{key:'nama',sortable:true},{key:'kota',sortable:true},{key:'provinsi',sortable:true},{key:'kodepos',sortable:true}],
+				filter: null,				
+			}
+	    },
+	}
+</script>
