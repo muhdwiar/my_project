c10bda278446bd788d62ed3e0d90beddb045d9c2
Muhamad Dwi Arifiant <muhdwi45@gmail.com>
2019-05-31 13:50:36 +0700

    Filter Menggunakan GraphQL(Bukan JS)

--- test_filter_nuxt_apollo/pages/_data_kota.vue
+++ test_filter_nuxt_apollo/pages/_data_kota.vue
@@ -15,13 +15,10 @@
 		<div align="center" class="py-3">
 			<b-form-group class="my-5 w-50">
 	          <b-input-group>
-	            <b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
-	            <b-input-group-append>
-	              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
-	            </b-input-group-append>
+	            <b-form-input v-model="searchword" placeholder="Type to Search"></b-form-input>
 	          </b-input-group>
 	        </b-form-group>
-			<b-table class="mb-5 w-75" striped bordered  :items="IDKota" :fields="fields" :filter="filter"></b-table>
+			<b-table class="mb-5 w-75" striped bordered  :items="IDKota" :fields="fields"></b-table>
 		</div>
 
 		<!-- FOOTER -->
@@ -32,19 +29,27 @@
 </template>
 
 <script>
-	import gql from 'graphql-tag'
-	import dataKota from '~/query/data_kota.gql'
+	import gql from 'graphql-tag';
+	import DATA_KOTA from '~/query/data_kota.gql';
 
 	export default {
-	    apollo: {
-			IDKota:{
-				query: dataKota,
-			},
-	    },
-	    data(){
+		
+		data(){
 	    	return {
 				fields: [{key:'id',sortable:true},{key:'nama',sortable:true},{key:'kota',sortable:true},{key:'provinsi',sortable:true},{key:'kodepos',sortable:true}],
-				filter: null,				
+				searchword:'',
+							
+			}
+	    },
+
+	    apollo: {
+			IDKota: {
+				query : DATA_KOTA,
+				variables() {
+					return{
+						search: this.searchword,
+					}
+				}
 			}
 	    },
 	}
